0b8c6b621dfe3fd3b312700cf6254c9baa64a8ec
Rohit Jadhav <jadhavrohit3004@gmail.com>
2024-06-24 00:24:32 +0530

    Contact class for AddressBook Program UC1

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,58 @@
-public class AddressBookMain{
+import java.util.ArrayList;
+import java.util.List;
+
+class Contact {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact: {" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", address='" + address + '\'' + ", city='" + city + '\'' + ", state='" + state + '\'' + ", zip='" + zip + '\'' + ", phoneNumber='" + phoneNumber + '\'' + ", email='" + email + '\'' + '}';
+    }
+}
+
+class AddressBook {
+    private List<Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void displayContacts() {
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
+}
+
+public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book Program");
+        AddressBook addressBook = new AddressBook();
+
+        Contact contact1 = new Contact("Rohit", "Jadhav", "Narayangaon, Junnar", "Pune", "Maharashtra", "410504", "8805503582", "jadhavrohit3004@gmail.com");
+        addressBook.addContact(contact1);
+
+        addressBook.displayContacts();
     }
 }
